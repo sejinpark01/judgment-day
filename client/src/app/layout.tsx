3e8e042d4fe149e476fe9d3d83cbf7834d0fcc08
sejinpark01 <parksejin.dev@gmail.com>
2026-04-06 22:34:58 +0900

    chore: Vercel Analytics 및 Speed Insights 도구 추가

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,9 +2,10 @@
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider"; // ✅ 추가
 import { Navbar } from "@/components/Navbar"; // ✅ Navbar 임포트 - Ver 2026.03.20
+import { Analytics } from "@vercel/analytics/next"  // Vercel 웹분석 추가 - Ver 2026.04.06
+import { SpeedInsights } from "@vercel/speed-insights/next" // Vercel 속도 측정 추가 - Ver 2026.04.06
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,12 +17,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     // ✅ suppressHydrationWarning 추가 필수!
-    <html lang="ko" suppressHydrationWarning> 
+    <html lang="ko" suppressHydrationWarning>
       <body className={inter.className}>
         {/* ✅ ThemeProvider로 감싸기 */}
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Navbar /> {/* ✅ 여기에 한 줄 추가! (모든 페이지 상단에 헤더 고정) */}
           {children}
+          <Analytics />       {/* 방문자 분석 센서 */}
+          <SpeedInsights />   {/* 속도 측정 센서 */}
         </ThemeProvider>
       </body>
     </html>
